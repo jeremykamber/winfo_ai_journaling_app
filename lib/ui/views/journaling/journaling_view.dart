a49f8f0a24052f47e8d696798d4967f6b65d0bb8
phildeleon <pleon@uw.edu>
2025-01-25 15:26:31 -0800

    started text area

--- lib/ui/views/journaling/journaling_view.dart
+++ lib/ui/views/journaling/journaling_view.dart
@@ -17,16 +17,30 @@ class JournalingView extends StackedView<JournalingViewModel> {
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-          child: Container(
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: Text('hello', style: TextStyle(
-              fontSize: 35,
-              fontWeight: FontWeight.w900,
-            ),),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Text('hello', style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w900,
+                ),),
+              ),
+              TextField(
+                obscureText: false, 
+                decoration: InputDecoration(
+                  hintText: 'Enter here',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  labelText: 'Today\'s Note',
+                  )
+                ) 
+            ],
           ),
             ),
           ),
